0303afb06fd034279ac0fb8c127904b5f7ba8542
zhouteng <wwwsousoucom@126.com>
2019-12-05 09:55:50 +0800

    string
    Took 41 seconds

--- doc/advance/ch01/string/main.go
+++ doc/advance/ch01/string/main.go
@@ -3,8 +3,49 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"unicode/utf8"
 	"unsafe"
 )
+
+func str2bytes(s string) []byte {
+	p := make([]byte, len(s))
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		p[i] = c
+	}
+	return p
+}
+
+func bytes2str(s []byte) (p string) {
+	data := make([]byte, len(s))
+	for i, c := range s {
+		data[i] = c
+	}
+	hdr := (*reflect.StringHeader)(unsafe.Pointer(&p))
+	hdr.Data = uintptr(unsafe.Pointer(&data[0]))
+	hdr.Len = len(s)
+	return p
+}
+
+func str2runes(s []byte) []rune {
+	var p []int32
+	for len(s) > 0 {
+		r, size := utf8.DecodeRune(s)
+		p = append(p, int32(r))
+		s = s[size:]
+	}
+	return []rune(p)
+}
+
+func runes2string(s []int32) string {
+	var p []byte
+	buf := make([]byte, 3)
+	for _, r := range s {
+		n := utf8.EncodeRune(buf, r)
+		p = append(p, buf[:n]...)
+	}
+	return string(p)
+}
 
 func main() {
 	s := "hello, world"
