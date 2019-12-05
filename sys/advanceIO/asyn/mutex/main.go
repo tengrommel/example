37a44bff565956288135cd21ac719366f976d7bb
zhouteng <wwwsousoucom@126.com>
2019-12-05 23:31:46 +0800

    原子性
    Took 34 minutes

--- sys/advanceIO/asyn/mutex/main.go
+++ sys/advanceIO/asyn/mutex/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 var total struct {
@@ -16,6 +17,66 @@ func worker(wg *sync.WaitGroup) {
 		total.Lock()
 		total.value += i
 		total.Unlock()
+	}
+}
+
+/**
+在worker的循环中，为了保证total.value += i 的原子性，我们通过sync.Mutex加锁和解锁来保证该语句在
+同一时刻只被一个线程访问。
+*/
+
+var totalValue uint64
+
+func workerValue(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i <= 100; i++ {
+		atomic.AddUint64(&totalValue, uint64(i))
+	}
+}
+
+type singleton struct{}
+
+var (
+	instance    *singleton
+	initialized uint32
+	mu          sync.Mutex
+	once        sync.Once
+)
+
+func Instance() *singleton {
+	if atomic.LoadUint32(&initialized) == 1 {
+		return instance
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	if instance == nil {
+		defer atomic.StoreUint32(&initialized, 1)
+		instance = &singleton{}
+	}
+	return instance
+}
+
+type Once struct {
+	m    sync.Mutex
+	done uint32
+}
+
+func InstanceOnce() *singleton {
+	once.Do(func() {
+		instance = &singleton{}
+	})
+	return instance
+}
+
+func (o *Once) Do(f func()) {
+	if atomic.LoadUint32(&o.done) == 1 {
+		return
+	}
+	o.m.Lock()
+	defer o.m.Unlock()
+	if o.done == 0 {
+		defer atomic.StoreUint32(&o.done, 1)
+		f()
 	}
 }
 
