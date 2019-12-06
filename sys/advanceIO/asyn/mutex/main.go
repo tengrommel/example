a8fe95bdc51534c75c84d95ffed3796a47d03fd8
zhouteng <wwwsousoucom@126.com>
2019-12-06 10:12:42 +0800

    订阅者模式
    Took 1 hour 43 minutes

--- sys/advanceIO/asyn/mutex/main.go
+++ sys/advanceIO/asyn/mutex/main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"sync"
 	"sync/atomic"
+	"syscall"
+	"time"
 )
+
+func Producer(factor int, out chan<- int) {
+	for i := 0; ; i++ {
+		out <- i * factor
+	}
+}
+
+func Consumer(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
+	}
+}
 
 var total struct {
 	sync.Mutex
@@ -87,4 +103,34 @@ func main() {
 	go worker(&wg)
 	wg.Wait()
 	fmt.Println(total.value)
+	done := make(chan int)
+	go func() {
+		println("你好，世界")
+		done <- 1
+	}()
+	<-done
+	// 当<-done执行时，必然要求done<-1也已经执行。
+	var mu sync.Mutex
+	go func() {
+		println("您好，世界")
+		mu.Unlock()
+	}()
+	done = make(chan int)
+	go func() {
+		fmt.Println("你好，世界")
+		<-done
+	}()
+	done <- 1
+	ch := make(chan int, 64)
+	go Producer(3, ch)
+	go Producer(5, ch)
+	go Consumer(ch)
+	time.Sleep(5 * time.Second)
+
+	ch1 := make(chan int, 64)
+	go Producer(3, ch1)
+	go Producer(5, ch1)
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Printf("quit (%v)\n", <-sig)
 }
