8da2ae60b89e90d48fc453fd2dab1ff13049cfa0
zhouteng <wwwsousoucom@126.com>
2020-01-02 21:45:09 +0800

    hash table
    Took 41 minutes

---
+++ example/master/hash-tables/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+/*
+Strictly speaking, a hash table is a data structure that stores one or more
+key-value pairs and uses a hash function to compute an index into an array of buckets or slots,
+from which the correct value can be discovered.
+Ideally, the hash function should assign each key to a unique bucket provided that you the required number of buckets,
+which is usually the case.
+*/
+
+const SIZE = 15
+
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+type HashTable struct {
+	Table map[int]*Node
+	Size  int
+}
+
+/**
+implementation of the hash function used in this particular hash
+The hashFunction() uses the modulo operator
+*/
+func hashFunction(i, size int) int {
+	return i % size
+}
+
+func insert(hash *HashTable, value int) int {
+	index := hashFunction(value, hash.Size)
+	element := Node{Value: value, Next: hash.Table[index]}
+	hash.Table[index] = &element
+	return index
+}
+
+func traverse(hash *HashTable) {
+	for k := range hash.Table {
+		if hash.Table[k] != nil {
+			t := hash.Table[k]
+			for t != nil {
+				fmt.Printf("%d -> ", t.Value)
+				t = t.Next
+			}
+			fmt.Println()
+		}
+	}
+}
+
+func main() {
+	table := make(map[int]*Node, SIZE)
+	hash := &HashTable{Table: table, Size: SIZE}
+	fmt.Println("Number of spaces: ", hash.Size)
+	for i := 0; i < 120; i++ {
+		insert(hash, i)
+	}
+	traverse(hash)
+}
