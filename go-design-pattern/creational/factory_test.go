77f3b2b2a22258ac8822b91f61bc7e123bd3dda5
zhouteng <wwwsousoucom@126.com>
2020-01-05 20:22:04 +0800

    工厂模式测试
    Took 1 hour 37 minutes

---
+++ go-design-pattern/creational/factory_test.go
@@ -0,0 +1,38 @@
+package creational
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGetPaymentMethodCash(t *testing.T) {
+	payment, err := GetPaymentMethods(Cash)
+	if err != nil {
+		t.Fatal("A payment method of type 'Cash' must ")
+	}
+	msg := payment.Pay(10.30)
+	if !strings.Contains(msg, "payed using cash") {
+		t.Error("The cash payment method message wasn't correct")
+	}
+	t.Log("LOG:", msg)
+}
+
+func TestGetPaymentMethodDebitCard(t *testing.T) {
+	payment, err := GetPaymentMethods(DebitCard)
+	if err != nil {
+		t.Fatal("A payment method of type 'DebitCard' must exist")
+	}
+	msg := payment.Pay(22.30)
+	if !strings.Contains(msg, "payed using debit card") {
+		t.Error("The debit card payment method message wasn't correct")
+	}
+	t.Log("LOG:", msg)
+}
+
+func TestGetPaymentMethodNonExistent(t *testing.T) {
+	_, err := GetPaymentMethods(20)
+	if err == nil {
+		t.Error("A payment method with ID 20 must return an error")
+	}
+	t.Log("LOG:", err)
+}
