997572429ca1778ac4d2230516d957e30ebd9512
zhouteng <wwwsousoucom@126.com>
2020-01-25 00:05:01 +0800

    encode
    Took 17 minutes

---
+++ example/go/net/tcp/ecode/protocol/protocol.go
@@ -0,0 +1,24 @@
+package protocol
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+// Encode 将消息编码
+func Encode(message string) ([]byte, error) {
+	// 读取消息的长度，转换成int32类型（占4个字节）
+	var length = int32(len(message))
+	var pkg = new(bytes.Buffer)
+	// 写入消息头
+	err := binary.Write(pkg, binary.LittleEndian, length)
+	if err != nil {
+		return nil, err
+	}
+	// 写入消息实体
+	err = binary.Write(pkg, binary.LittleEndian, []byte(message))
+	if err != nil {
+		return nil, err
+	}
+	return pkg.Bytes(), nil
+}
