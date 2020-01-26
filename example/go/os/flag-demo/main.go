4a31fc3dc4a720d311839141ed15d7c78f2a803e
zhouteng <wwwsousoucom@126.com>
2020-01-26 23:38:13 +0800

    ok
    Took 10 minutes

--- example/go/os/flag-demo/main.go
+++ example/go/os/flag-demo/main.go
@@ -9,13 +9,14 @@ import (
 // flag获取命令行参数
 func main() {
 	// 创建一个标志位参数
-	name := flag.String("name", "网页", "请输入名字")
+	var name string
+	flag.StringVar(&name, "name", "网页", "请输入名字")
 	age := flag.Int("age", 90, "请输入真实年龄")
 	married := flag.Bool("married", false, "结婚了吗")
 	cTime := flag.Duration("ct", time.Second, "结婚多久了")
 	// 使用flag
 	flag.Parse()
-	fmt.Println(*name)
+	fmt.Println(name)
 	fmt.Println(*age)
 	fmt.Println(*married)
 	fmt.Println(*cTime)
