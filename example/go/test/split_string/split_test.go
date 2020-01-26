e2a74d05f02cbd5fbe0bb80dacde38c524f35f77
zhouteng <wwwsousoucom@126.com>
2020-01-26 21:18:28 +0800

    如果需要报错详细提醒
    Took 12 minutes

--- example/go/test/split_string/split_test.go
+++ example/go/test/split_string/split_test.go
@@ -30,3 +30,42 @@ func Test3Split(t *testing.T) {
 		t.Fatalf("want: %v but got:%v\n", want, ret)
 	}
 }
+
+func TestS(t *testing.T) {
+	type testCase struct {
+		str  string
+		sep  string
+		want []string
+	}
+	testGroup := []testCase{
+		{"abc", "b", []string{"a", "c"}},
+		{"abbc", "bb", []string{"a", "c"}},
+	}
+
+	for _, item := range testGroup {
+		got := Split(item.str, item.sep)
+		if !reflect.DeepEqual(got, item.want) {
+			t.Fatalf("eant: %#v got: %#v", item.want, got)
+		}
+	}
+}
+
+func TestMapSplit(t *testing.T) {
+	type test struct { // 定义test结构体
+		input string
+		sep   string
+		want  []string
+	}
+	tests := map[string]test{ // 测试用例使用map存储
+		"simple":      {input: "a:b:c", sep: ":", want: []string{"a", "b", "c"}},
+		"wrong sep":   {input: "a:b:c", sep: ",", want: []string{"a:b:c"}},
+		"more sep":    {input: "abcd", sep: "bc", want: []string{"a", "d"}},
+		"leading sep": {input: "沙河有沙又有河", sep: "沙", want: []string{"", "河有", "又有河"}},
+	}
+	for name, tc := range tests {
+		got := Split(tc.input, tc.sep)
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("name:%s excepted:%#v, got:%#v", name, tc.want, got) // 将测试用例的name格式化输出
+		}
+	}
+}
