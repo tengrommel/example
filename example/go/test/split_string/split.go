27f0e0c8cf428446a8cccc940d91a4897178435e
zhouteng <wwwsousoucom@126.com>
2020-01-26 21:03:55 +0800

    ok
    Took 25 minutes

--- example/go/test/split_string/split.go
+++ example/go/test/split_string/split.go
@@ -9,7 +9,7 @@ func Split(str string, sep string) []string {
 	index := strings.Index(str, sep)
 	for index >= 0 {
 		ret = append(ret, str[:index])
-		str = str[index+1:]
+		str = str[index+len(sep):]
 		index = strings.Index(str, sep)
 	}
 	ret = append(ret, str)
--- example/go/test/split_string/split_test.go
+++ example/go/test/split_string/split_test.go
@@ -22,3 +22,11 @@ func Test2Split(t *testing.T) {
 		t.Errorf("want: %v but got: %v", want, ret)
 	}
 }
+
+func Test3Split(t *testing.T) {
+	ret := Split("abcef", "bc")
+	want := []string{"a", "ef"}
+	if !reflect.DeepEqual(ret, want) {
+		t.Fatalf("want: %v but got:%v\n", want, ret)
+	}
+}
