b1b523ac7b5103d13cbf084fe6fe74cd668f71d5
zhouteng <wwwsousoucom@126.com>
2020-02-03 20:49:24 +0800

    选项设计模式
    Took 20 minutes

--- example/session/demo/main.go
+++ example/session/demo/main.go
@@ -9,22 +9,64 @@ type Options struct {
 	intOption1 int
 	intOption2 int
 	intOption3 int
-	abc        int
 }
 
+// 声明一个函数类型的变量，用于传参
+type Option func(opts *Options)
+
 // 初始化结构体
-func InitOptions1(strOption1, strOption2, strOption3 string,
-	intOption1, intOption2, intOption3 int) {
-	options := Options{}
-	options.strOption1 = strOption1
-	options.strOption2 = strOption2
-	options.strOption3 = strOption3
-	options.intOption1 = intOption1
-	options.intOption2 = intOption2
-	options.intOption3 = intOption3
-	fmt.Printf("init option %#v\n", options)
+func InitOptions(opts ...Option) {
+	options := &Options{}
+	// 遍历opts，得到每一个函数
+	for _, opt := range opts {
+		// 调用函数，在函数里，给穿进去的对象赋值
+		opt(options)
+	}
+	fmt.Printf("init options %#v\n", options)
+}
+
+// 定义具体给某个字段赋值的方法
+func WithStrOption1(str string) Option {
+	return func(opts *Options) {
+		opts.strOption1 = str
+	}
+}
+
+// 定义具体给某个字段赋值的方法
+func WithStrOption2(str string) Option {
+	return func(opts *Options) {
+		opts.strOption2 = str
+	}
+}
+
+// 定义具体给某个字段赋值的方法
+func WithStrOption3(str string) Option {
+	return func(opts *Options) {
+		opts.strOption3 = str
+	}
+}
+
+// 定义具体给某个字段赋值的方法
+func WithIntOption1(i int) Option {
+	return func(opts *Options) {
+		opts.intOption1 = i
+	}
+}
+
+// 定义具体给某个字段赋值的方法
+func WithIntOption2(i int) Option {
+	return func(opts *Options) {
+		opts.intOption2 = i
+	}
+}
+
+// 定义具体给某个字段赋值的方法
+func WithIntOption3(i int) Option {
+	return func(opts *Options) {
+		opts.intOption3 = i
+	}
 }
 
 func main() {
-
+	InitOptions(WithStrOption1("str1"), WithStrOption1("str2"))
 }
