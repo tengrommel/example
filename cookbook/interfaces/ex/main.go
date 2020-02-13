57dc22378b277caf6f35ae4146ffc5c212e9991f
zhouteng <wwwsousoucom@126.com>
2020-02-13 13:10:08 +0800

    ok
    Took 1 hour 11 minutes

--- cookbook/interfaces/ex/main.go
+++ cookbook/interfaces/ex/main.go
@@ -6,9 +6,15 @@ import (
 	"fmt"
 )
 
+// The Copy() function copies bytes between interfaces and treats that data like a stream.
+// Thinking of data as streams has a lot of practical uses, especially when working with
+// network traffic or filesystems. The Copy() function also creates a MultiWriter
+// interface that combines two writer streams and writes
+// to them twice using ReadSeeker.
+
 func main() {
 	in := bytes.NewReader([]byte("example"))
-	out := bytes.Buffer{}
+	out := &bytes.Buffer{}
 	fmt.Print("stdout on Copy = ")
 	if err := interfaces.Copy(in, out); err != nil {
 		panic(err)
