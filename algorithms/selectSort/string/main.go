1700a95f5cce224bc57282995a863b2acb5a4b92
zhouteng <wwwsousoucom@126.com>
2020-02-16 14:52:44 +0800

    string 比较
    Took 25 minutes

--- algorithms/selectSort/string/main.go
+++ algorithms/selectSort/string/main.go
@@ -1,13 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func SelectSortMaxString(arr []string) string {
+	length := len(arr) // 数组的长度
+	if length <= 1 {
+		return arr[0] // 一个元素的数组，直接返回
+	} else {
+		max := arr[0] // 假定第一个最大
+		for i := 1; i < length; i++ {
+			if strings.Compare(arr[i], max) < 0 {
+				max = arr[i]
+			}
+			//if arr[i] > max { // 任何一个比我大的数，最大的
+			//	max = arr[i]
+			//}
+		}
+		return max
+	}
+}
+
+func SelectSort(arr []string) []string {
+	length := len(arr)
+	if length <= 1 {
+		return arr
+	} else {
+		for i := 0; i < length-1; i++ { // 只剩一个元素不需要挑选
+			min := i // 标记索引
+			for j := i + 1; j < length; j++ {
+				if arr[min] < arr[j] {
+					min = j
+				}
+			}
+			if i != min {
+				arr[i], arr[min] = arr[min], arr[i]
+			}
+		}
+		return arr
+	}
+}
 
 func main() {
-	fmt.Println("a" > "b")
-	fmt.Println("a" < "b")
-	fmt.Println("a1" == "a")
-	pa := "a"
-	pb := "b"
-	fmt.Println(&pa, &pb)
-	fmt.Println(pa < pb)
+	arr := []string{"c", "a", "b", "x"}
+	fmt.Println(SelectSort(arr))
 }
