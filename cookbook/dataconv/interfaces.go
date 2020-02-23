caa0444655e81121a8a1342f12110fcdd9c454e0
zhouteng <wwwsousoucom@126.com>
2020-02-23 17:14:33 +0800

    interface
    Took 24 minutes

---
+++ cookbook/dataconv/interfaces.go
@@ -0,0 +1,31 @@
+package dataconv
+
+import "fmt"
+
+// CheckType will print based on the interface type
+func CheckType(s interface{}) {
+	switch s.(type) {
+	case string:
+		fmt.Println("It's a string!")
+	case int:
+		fmt.Println("It' an int!")
+	default:
+		fmt.Println("not sure what it is ....")
+	}
+}
+
+func Interfaces() {
+	CheckType("test")
+	CheckType(1)
+	CheckType(false)
+	var i interface{}
+	i = "test"
+	// manually check an interface
+	if val, ok := i.(string); ok {
+		fmt.Println("val is", val)
+	}
+	// this one should fail
+	if _, ok := i.(int); !ok {
+		fmt.Println("uh oh! glad we handled this")
+	}
+}
